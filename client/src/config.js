014f787085bda61deaf2dcc0f931c4f92e68a809
github username <github email address>
2018-09-21 11:00:59 -0400

    updated opp

--- client/src/config.js
+++ client/src/config.js
@@ -5,7 +5,7 @@ export default {
   demFECId: 'H8PA06087',
   demCommitteeFECId: 'C00637371',
   repName: 'Greg Mccauley',
-  repTwitter: 'gregmccauleyusa',
+  repTwitter: 'McCauley4PA',
   repMemberId: null,
   repFECId: 'H8PA07192',
   repCommitteeFECId: 'C00668400'
